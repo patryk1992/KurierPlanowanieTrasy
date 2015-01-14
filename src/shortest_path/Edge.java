c3e1aab5a3c21140baed1e64b61125f51ea10c2c
Zbigniew Reslinski <zbigniew.reslinski@gmail.com>
2015-01-14 21:08:20 +0100

    .

---
+++ src/shortest_path/Edge.java
@@ -0,0 +1,11 @@
+package shortest_path;
+
+class Edge {
+	public final Vertex target;
+	public final double weight;
+
+	public Edge(Vertex argTarget, double argWeight) {
+		target = argTarget;
+		weight = argWeight;
+	}
+}
